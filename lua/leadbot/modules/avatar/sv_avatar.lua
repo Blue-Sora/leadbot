8ce07abc2457cfbdc48fd8e4b43525100e934cec
Blue-Sora <70980584+Blue-Sora@users.noreply.github.com>
2022-09-06 19:04:39 -0300

    Add files via upload

--- lua/leadbot/modules/avatar/sv_avatar.lua
+++ lua/leadbot/modules/avatar/sv_avatar.lua
@@ -1,5 +1,9 @@
 local oldAddBot = LeadBot.AddBotOverride
 
+
+-- We should actually do this for clientside... since some players might not want this enabled and others do.
+CreateConVar("leadbot_renderAvatars", "1", {FCVAR_ARCHIVE, FCVAR_NOTIFY}, "Bots will have their profile pictures on scoreboard as their avatars.")
+
 function LeadBot.AddBotOverride(bot)
     oldAddBot(bot)
 
