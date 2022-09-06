6a19582874bb54de514277091cfbf40ad5fd9099
Blue-Sora <70980584+Blue-Sora@users.noreply.github.com>
2022-09-06 19:02:48 -0300

    Add files via upload

--- lua/entities/leadbot_navigator.lua
+++ lua/entities/leadbot_navigator.lua
@@ -41,10 +41,11 @@ function ENT:Initialize()
 	self.lookAngleExtraSinDifference = 0 -- Not really much important, it just makes it have a difference sin value by increasing the value above to make a difference between other players cuz' I'm dumb.
 
 	-- Walking
-	self.walkFast = true -- Boolean, if true the bot will walk fast, otherwise false, not walk fast (sprint).
+	--self.walkFast = true -- Boolean, if true the bot will walk fast, otherwise false, not walk fast (sprint).
 	self.nextWalkNormal = CurTime() + 1 -- Timer for the next walk normal to stop walking/spriting fast.
-	self.walkNormalTimer = 0 -- Timer till it stops walking normally and then sprints again.
+	self.startWalkingFastTimer = 0 -- Timer till it stops walking normally and then sprints again.
 	self.movingAngle = Angle(0, 0, 0)
+	self.WalkType = 0 -- 0 = Walk normal, 1 = Sprint, 2 = Walk very slowly
 
 	-- Weapons
 	self.nextWeaponSwitch = CurTime() + 0.1
@@ -62,66 +63,14 @@ function ENT:Initialize()
 	self.isTargettable = true
 
 
+	-- Chatting behaviours
+	self.AllowedToChat = true -- Maybe we would want a specific bot to not chat...?
+
 	self:CReset()
 
 	if LeadBot.AddControllerOverride then
 		LeadBot.AddControllerOverride(self)
 	end
-end
-
-function ENT:CReset()
-	-- Weapons
-	self.nextWeaponSwitch = CurTime() + 0.1
-	
-	-- Battle behaviours
-	self.battle_behaviourType = 0
-	self.battle_nextBehaviourTime = CurTime() + 0.1
-
-	-- Boolean
-	self.canSeeTarget = false
-	self.IGuessWeAreAiming = false
-
-	-- Targetting
-	self.target_timeToRealize = 0 -- This is the timer that takes the bot to realize that there might be an enemy right on our front and target them.
-	self.target_timeToAim = 0 -- Timer that it takes to aim at the direction it saw the enemy when it weren't targetted and aimed before.
-	self.Target = nil
-	self.IgnoreTarget = false -- Boolean, should we ignore our attack even though we have it?
-
-	-- Enemies
-	self.lastSeenEnemiesPos = {}
-
-	-- Counter
-	self.FrameCounter = 0
-
-	-- Following
-	self.followingPlayer = nil
-
-	-- Sniper
-	self.sniper_preparingForNextShotTime = 0
-
-
-	------------------------------------------------------------------
-	-- Specific Weapons
-	------------------------------------------------------------------	
-
-	-- Physgun
-	self.Physgun_TargetProp = nil -- Current prop that we wanna get
-	self.Physgun_NextLookForProp = 0 -- Timer to look for near props to get so it doesn't have to do this every frame.
-	self.Physgun_TargetPropGiveUpTimer = 0 -- Timer till it gives up after finding one and trying to get it
-
-	return true
-end
-
-function ENT:CSetTarget(target)
-
-
-	self.Target = target
-	self.ForgetTarget = CurTime() + 2
-end
-
-function ENT:CSetGoal(goalvec)
-	self.PosGen = goalvec
-	self.LastSegmented = CurTime() + 6
 end
 
 function ENT:ChasePos()
@@ -162,7 +111,6 @@ end
 function ENT:OnRemove()
 
 end
-
 -- remade this in lua so we can finally ignore the controller's bot
 -- for some reason it's not really possible to overwrite IsAbleToSee
 local function PointWithinViewAngle(pos, targetpos, lookdir, fov)
@@ -245,4 +193,138 @@ function ENT:RunBehaviour()
 
 		coroutine.yield()
 	end
+end
+
+
+function ENT:HRun(hk, args)
+	local weGotTrue = false
+
+	
+	for i, h in ipairs(hk) do
+		if h(args) then
+			weGotTrue = true
+		end
+	end
+
+	return weGotTrue
+end
+
+function ENT:CReset()
+
+	-- Weapons
+	self.nextWeaponSwitch = CurTime() + 0.1
+	
+	-- Battle behaviours
+	self.battle_behaviourType = 0
+	self.battle_nextBehaviourTime = CurTime() + 0.1
+
+	-- Boolean
+	self.canSeeTarget = false
+	self.IGuessWeAreAiming = false
+
+	-- Targetting
+	self.target_timeToRealize = 0 -- This is the timer that takes the bot to realize that there might be an enemy right on our front and target them.
+	self.target_timeToAim = 0 -- Timer that it takes to aim at the direction it saw the enemy when it weren't targetted and aimed before.
+	self.Target = nil
+	self.IgnoreTarget = false -- Boolean, should we ignore our attack even though we have it?
+
+	self.LooseTargetTimer = 0
+
+	-- Enemies
+	self.lastSeenEnemiesPos = {}
+
+	-- Counter
+	self.FrameCounter = 0
+
+	-- Following
+	self.followingPlayer = nil
+	self.followingPlayerDistance = 20110 -- Distance to be from the following player to not try to interrupt them.
+	self.followingPlayerNextThink = 0 -- If follwoing a player, this will be the next think for us to "see" that they moved or changed positions, basically like a delay to notice that they moved for realism.
+	self.followingPlayerNextPositionChange = 0 -- If the player is stopped, this will be the timer till we change positions from where we are waiting for them to move to another one close to them.
+
+	-- Sniper
+	self.sniper_preparingForNextShotTime = 0
+
+	-- Chat
+	self.ChatIsTyping = false
+
+	------------------------------------------------------------------
+	-- Navmesh
+	------------------------------------------------------------------	
+	self.NavTimerSinceCouldntFindNav = CurTime() -- Timer since we couldn't find a goal through nav.
+	self.NavTimerSinceCouldntFindNavTimerToKill = 0 -- After reaching the timer since we couldn't find any navs, this will be the timer till we kill ourself.
+
+	------------------------------------------------------------------
+	-- Goal
+	------------------------------------------------------------------	
+	self.goalPos = Vector(0, 0, 0)
+	self.LastSegmented = 0
+
+	------------------------------------------------------------------
+	-- Specific Weapons
+	------------------------------------------------------------------	
+
+	-- Physgun
+	self.Physgun_TargetProp = nil -- Current prop that we wanna get
+	self.Physgun_NextLookForProp = 0 -- Timer to look for near props to get so it doesn't have to do this every frame.
+	self.Physgun_TargetPropGiveUpTimer = 0 -- Timer till it gives up after finding one and trying to get it
+
+
+	hook.Run("LeadBot_OnBotNavigatorReset", bot, self)
+	return true
+end
+
+function ENT:CSetTarget(target)
+
+
+	self.Target = target
+	self.ForgetTarget = CurTime() + 2
+
+
+	hook.Run("LeadBot_OnBotTarget", self:GetOwner(), target)
+end
+function ENT:CLooseTarget()
+
+	hook.Run("LeadBot_OnBotTargetLoose", self:GetOwner(), self.Target)
+
+	self.Target = nil
+	self.canSeeTarget = false
+end
+
+function ENT:CSetGoal(goalvec)
+	self.PosGen = goalvec
+	self.LastSegmented = CurTime() + 6
+end
+function ENT:GetTarget()
+
+	return self.Target
+end
+function ENT:HasTarget()
+
+	return IsValid(self.Target)
+end
+
+function ENT:FollowTarget( target, IfAlreadyKeep )
+
+	if !IfAlreadyKeep then IfAlreadyKeep = false else IfAlreadyKeep = true end
+
+	local IsAlreadyFollowing = IsValid( self.followingPlayer )
+
+	if !IfAlreadyKeep and IsAlreadyFollowing then self:FollowStop() return end
+
+
+	if IsValid( target ) then
+		self.followingPlayer = target
+	end
+end
+function ENT:FollowStop( target )
+
+	self.followingPlayer = nil
+end
+function ENT:IsFollowingPlayer( ply )
+
+	return IsValid( self.followingPlayer )
+end
+function ENT:FollowGetTarget()
+	return self.followingPlayer
 end
